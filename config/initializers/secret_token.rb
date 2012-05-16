6bd187441171c0cf8af412be7d50de467b083fe8
darren <darren@new-host-4.home>
2012-05-15 22:42:28 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railstoot::Application.config.secret_token = 'a84fb7335ec6860946b76495b8a6ce82d29415f5dd0edfa6f92df0cb4323f7ff195517878ca130ba3f81768d48761692112b81a04733037d402250dd72ae118f'
